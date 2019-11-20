91b8f1cecf48b4457b7737964931971b0ea5c709
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 19:33:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     elsif @sender.valid? && @receiver.valid? && @status == "pending"
       @sender.balance -= @amount
-      @receiver.deposit(@amount) += @amount
+      @receiver.deposit(@amount)
       @status = "complete"
    end
   end
