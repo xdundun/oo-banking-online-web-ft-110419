81cbabaf34105182d67221da1d5ce98196ad6c5e
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 19:53:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,4 +22,11 @@ class Transfer
         return "Transaction rejected. Please check your account balance."
       end
   end
+
+  def reverse_transfer
+    if self.status =="complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+end
+
 end #end of class
