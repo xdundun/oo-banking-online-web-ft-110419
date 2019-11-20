7041e1f9415dd069c63946efa59b0aa17f343fd2
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 19:30:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,13 +18,14 @@ class Transfer
 
   def execute_transaction
 
-    if @sender.valid? && @receiver.valid? && @status == "pending"
+    if @sender.status == "closed"
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @sender.valid? && @receiver.valid? && @status == "pending"
       @sender.balance -= amount
       @receiver.balance += amount
       @status = "complete"
-    elsif @sender.status == "closed"
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
    end
   end
 end #end of class
