8a09e2efe8aeae04a65d313db295b56b8e2d6f38
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 19:16:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,7 @@
 class Transfer
   attr_accessor :transfer, :receiver, :status
 
-  def initialize(sender,receiver, status = "pending")
-end
+  def initialize(sender,receiver, status = "pending",amount)
+
+  end
 end
